9cbbf3875a9d1bfd9adfb41b46389c34437d0760
IvanB <lthefabulousl@gmail.com>
2020-06-14 17:05:37 +0300

    Updated custom exception to use the parent constructor

--- src/test/java/com/helpers/util/customExceptions/InvalidPlatformException.java
+++ src/test/java/com/helpers/util/customExceptions/InvalidPlatformException.java
@@ -3,12 +3,8 @@ package com.helpers.util.customExceptions;
 public class InvalidPlatformException extends Exception {
     public InvalidPlatformException(String platform){
 
-        super();
-
-        System.out.println("\n !!! Invalid platform argument \"" + platform + "\". Please specify either \"win\" for Windows or \"mac\" for Mac OS. !!!");
-
-        System.out.println("See below Examples for more info:\n" +
-                "To run tests on Windows platform, use VM argument \"-Dplatform=win\" or run maven goal with \"mvn test -Dplatform=win\"\n" +
-                "To run tests in Mac OS platform, use argument \"-Dplatform=mac\" or run maven goal with \"mvn test -Dplatform=mac\"");
+        super("\n!!! Invalid platform argument \"" + platform + "\". Please specify either \"win\" for Windows or \"mac\" for Mac OS. !!!\n\nSee below Examples for more info:\n" +
+                                "To run the tests on Windows platform, please use VM argument \"-Dplatform=win\" or run maven goal with \"mvn test -Dplatform=win\"\n" +
+                                "To run the tests on Mac OS platform, please use argument \"-Dplatform=mac\" or run maven goal with \"mvn test -Dplatform=mac\"\n");
     }
 }
